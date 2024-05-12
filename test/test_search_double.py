051726697e0a1110afd15e39545e3e8e04ddbd4b
Sergeev Pavel <p.u.sergeev@gmail.com>
2024-05-12 06:23:55 +0300

    1

---
+++ test/test_search_double.py
@@ -0,0 +1,17 @@
+import requests
+
+
+city_ids = []
+for page_num in range(1, 5):
+    query = {
+        "page": page_num,
+    }
+    data = requests.get('https://regions-test.2gis.com/1.0/regions', params=query).json()
+    for i, item in enumerate(data['items']):
+        city_id = item['id']
+        city_ids.append(city_id)
+print(city_ids, end=' ')
+print()
+print(len(city_ids))
+print(len(set(city_ids)))
+assert len(city_ids) == len(set(city_ids))
---
+++ test/test_total_count.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def test_total_count():
+ query = {"page": "1"}
+ response = requests.get('https://regions-test.2gis.com/1.0/regions', params=query)
+ data = response.json()
+ keys = data['items']
+
+ query = {"page": "2"}
+ response = requests.get('https://regions-test.2gis.com/1.0/regions', params=query)
+ data = response.json()
+ keys2 = data['items']
+
+ query = {"page": "3"}
+ response = requests.get('https://regions-test.2gis.com/1.0/regions', params=query)
+ data = response.json()
+ keys3 = data['items']
+ total_item = (len(keys) + len(keys2) + len(keys3))
+ assert data['total'] == total_item, 'Несоответствие количества регионов'
+
